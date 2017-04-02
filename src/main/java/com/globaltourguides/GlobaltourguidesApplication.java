8884887590ff1d9435895d9178a2b5d38760955f
jie <jchen74uwmadisonaws@gmail.com>
2017-04-01 19:22:30 -0600

    Initial project commit.

---
+++ src/main/java/com/globaltourguides/GlobaltourguidesApplication.java
@@ -0,0 +1,12 @@
+package com.globaltourguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobaltourguidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobaltourguidesApplication.class, args);
+	}
+}
